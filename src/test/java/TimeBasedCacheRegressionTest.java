0d716ec0752cd9f069e24dfa928d6a3d3268ec50
amit <amitwadhwa83@gmail.com>
2025-02-07 19:49:22 +0100

    adding more test cases

--- src/test/java/TimeBasedCacheRegressionTest.java
+++ src/test/java/TimeBasedCacheRegressionTest.java
@@ -1,8 +1,9 @@
+// src/test/java/TimeBasedCacheRegressionTest.java
 import org.example.TimeBasedCache;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TimeBasedCacheTest {
+public class TimeBasedCacheRegressionTest {
 
     @Test
     public void testPutAndGet() {
@@ -57,38 +58,4 @@ public class TimeBasedCacheTest {
         assertEquals("value1", cache.get("key1"));
         assertEquals("value2", cache.get("key2"));
     }
-
-
-    @Test
-    public void testCacheIntegration() throws InterruptedException {
-        TimeBasedCache<String, String> cache = new TimeBasedCache<>(1000, 3);
-
-        // Simulate adding entries
-        cache.put("key1", "value1", 2000);
-        cache.put("key2", "value2", 3000);
-        cache.put("key3", "value3", 4000);
-
-        // Verify entries are added
-        assertEquals("value1", cache.get("key1"));
-        assertEquals("value2", cache.get("key2"));
-        assertEquals("value3", cache.get("key3"));
-
-        // Wait for some entries to expire
-        Thread.sleep(2500);
-
-        // Verify expired entries are removed
-        assertNull(cache.get("key1"));
-        assertEquals("value2", cache.get("key2"));
-        assertEquals("value3", cache.get("key3"));
-
-        // Add another entry to trigger eviction
-        cache.put("key4", "value4", 2000);
-
-        // Verify eviction of the oldest entry
-        assertNull(cache.get("key2"));
-        assertEquals("value3", cache.get("key3"));
-        assertEquals("value4", cache.get("key4"));
-    }
-
-
 }
